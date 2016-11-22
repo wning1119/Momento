014595f221363bf6d7d9df81a9b6eb3004a47247
HenLee <Henrrlee@gmail.com>
2016-11-21 17:02:01 -0800

    Add files via upload
    Java: Capture Image or Add from gallery when creating post

--- app/src/main/java/com/example/ningwang/momento/NewPostActivity.java
+++ app/src/main/java/com/example/ningwang/momento/NewPostActivity.java
@@ -1,12 +1,23 @@
 package com.example.ningwang.momento;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class NewPostActivity extends AppCompatActivity {
+
+    public static final int CAMERA_REQUEST = 10;
+    public static final int GALLERY_REQUEST = 11;
+    private ImageView imgSpecimentPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,45 @@ public class NewPostActivity extends AppCompatActivity {
                 goToMainActivity();
             }
         });
+
+        imgSpecimentPhoto = (ImageView) findViewById(R.id.imgSpecimenPhoto);
     }
     private void goToMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void btnTakePhotoClicked(View v){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    public void btnGalleryClicked(View v){
+        Intent galleryIntent = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST) {
+                Bitmap cameraImage = (Bitmap) data.getExtras().get("data");
+                imgSpecimentPhoto.setImageBitmap(cameraImage);
+            }
+
+            if(requestCode == GALLERY_REQUEST){
+                Uri imageUri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    imgSpecimentPhoto.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }

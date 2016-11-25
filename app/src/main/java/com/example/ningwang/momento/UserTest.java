a4f792f16cce9a95914d554747356192034a7c38
SongyanXie <xiesongyan1992@163.com>
2016-11-25 11:19:23 -0800

    JUnit test for User

---
+++ app/src/main/java/com/example/ningwang/momento/UserTest.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by Think on 2016/11/25.
+ */
+public class UserTest extends TestCase {
+    private static User user;
+
+    @BeforeClass
+    public void setUp()
+    {
+        user = new User();
+    }
+
+    @Test
+    public void testUserInfo()
+    {
+        user.setUserInfo(1, "a");
+        assertEquals(1, user.getUser_id());
+        assertEquals("a", user.getUser_name());
+    }
+}

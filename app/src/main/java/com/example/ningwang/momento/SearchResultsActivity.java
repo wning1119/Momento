e5abfa96174e5d2bbb1bd201b31014c98a551479
lucyzhang17 <lucyz17@ucla.edu>
2016-11-26 13:54:36 -0800

    Add files via upload

---
+++ app/src/main/java/com/example/ningwang/momento/SearchResultsActivity.java
@@ -0,0 +1,42 @@
+// check import list later
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class SearchResultsActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            showResults(query);
+        }
+    }
+
+    private void showResults(String query) {
+        // Query your data set and show results
+        // ...
+    }
+}

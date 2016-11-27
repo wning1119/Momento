9259fbdba57d7a44723cb022eef3462ef008d916
lucyzhang17 <lucyz17@ucla.edu>
2016-11-27 15:56:49 -0800

    Update Post.Java
    Incorporate database hooks.

--- app/src/main/java/com/example/ningwang/momento/Post.java
+++ app/src/main/java/com/example/ningwang/momento/Post.java
@@ -4,151 +4,15 @@ import java.lang.reflect.Array;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
 
 /**
  * Created by Songyan Xie on 2016/11/13.
  */
 
 // Post Class
+@DynamoDBTable(tableName = "Posts")
 public class Post {
-    public Post()
-    {
-        favorite = 0;
-        timeout = 1;
-        subject = null;
-        detail = null;
-        timestamp = null;
-        longitude = 0.0;
-        latitude = 0.0;
-        category = new ArrayList<>();
-        replies = new ArrayList<>();
-        ownerId = "";
-        id = -1;
-    }
-
-    public Post(int favorite, int timeout, String subject, String detail,
-                Timestamp timestamp, double longitude, double latitude,
-                ArrayList<String> category, ArrayList<Reply> replies,
-                String ownerId, int id)
-    {
-        this.favorite = favorite;
-        this.timeout = timeout;
-        this.subject = subject;
-        this.detail = detail;
-        this.timestamp = timestamp;
-        this.longitude = longitude;
-        this.latitude = latitude;
-        this.category = category;
-        this.replies = replies;
-        this.ownerId = ownerId;
-        this.id = id;
-    }
-
-    public void setOwner(String ownerId)
-    {
-        this.ownerId = ownerId;
-    }
-
-    public String getOwner() { return ownerId; }
-
-    // get # of favorites
-    public int getFavorite() { return favorite; }
-
-    // increase # of favorites
-    public void increaseFavorite() { favorite++; }
-
-    // decrease # of favorites
-    public void decreaseFavorite()
-    {
-        if (favorite > 0)
-            favorite--;
-    }
-
-    public void setFavorite(int favorite) { this.favorite = favorite; }
-
-    public void setCategory(ArrayList<String> category) { this.category = category; }
-
-    // set timeout
-    public void setTimeout(int timeout) { this.timeout = timeout; }
-
-    // get timeout
-    public int getTimeout() { return timeout; }
-
-    // increase timeout
-    public void increaseTimeout() { timeout++; }
-
-    // decrease timeout
-    public void decreaseTimeout()
-    {
-        if (timeout > 0)
-            timeout--;
-    }
-
-    // set subject
-    public void setSubject(String subject) { this.subject = subject; }
-
-    // get subject
-    public String getSubject() { return subject; }
-
-    // set detail
-    public void setDetail(String detail) { this.detail = detail; }
-
-    // get detail
-    public String getDetail() { return detail; }
-
-    // set timestamp
-    public void setTimestamp()
-    {
-        Date date = new Date();
-        timestamp = new Timestamp(date.getTime());
-    }
-
-    // get timestamp
-    public Timestamp getTimestamp() { return timestamp; }
-
-    // set location
-    public void setLocation(double longitude, double latitude)
-    {
-        this.longitude = longitude;
-        this.latitude = latitude;
-    }
-
-    // get longitude
-    public double getLongitude() { return longitude; }
-
-    // get latitude
-    public double getLatitude() { return latitude; }
-
-    // add contents to category
-    public void addToCategory(String content)
-    {
-        category.add(content);
-    }
-
-    // get category
-    public ArrayList<String> getCategory()
-    {
-        return category;
-    }
-
-    // add replies
-    public void addToReplies(Reply reply)
-    {
-        replies.add(reply);
-    }
-
-    // get replies
-    public ArrayList<Reply> getReplies()
-    {
-        return replies;
-    }
-
-    // set id
-    public void setId(int id) { this.id = id; }
-
-    // get id
-    public int getId() { return id; }
-
     // private fields
     private int favorite;
     private int timeout;
@@ -158,8 +22,128 @@ public class Post {
     private double longitude;
     private double latitude;
     private ArrayList<String> category;
+    private ArrayList<int> replyIds;
     private ArrayList<Reply> replies;
     private String ownerId;
     private int id;
 
+    @DynamoDBHashKey(attributeName = "postId")
+    // get id
+    public int getId() { return id; }
+    // set id
+    public void setId(int id) { this.id = id; }
+
+    @DynamoDBAttribute(attributeName = "Timestamp")
+    // get timestamp
+    public Timestamp getTimestamp() { return timestamp; }
+    // set timestamp
+    public void setTimestamp()
+    {
+        Date date = new Date();
+        tp = new Timestamp(date.getTime());
+        this.timestamp = tp;
+    }
+
+    @DynamoDBAttribute(attributeName = "category")
+    // get category
+    public ArrayList<String> getCategory() { return category; }
+    public void setCategory(ArrayList<String> category) { this.category = category; }
+    // add contents to category
+    public void addToCategory(String content)
+    {
+        ArrayList<String> currCategories = getCategory();
+        currCategories.add(content);
+        setCategory(currCategories);
+    }
+
+    @DynamoDBAttribute(attributeName = "detail")
+    // get detail
+    public String getDetail() { return detail; }
+    // set detail
+    public void setDetail(String detail) { this.detail = detail; }
+
+    @DynamoDBAttribute(attributeName = "favorite")
+    // get # of favorites
+    public int getFavorite() { return favorite; }
+    // set # of favorites
+    public void setFavorite(int favorite) { this.favorite = favorite; }
+    // increase # of favorites
+    public void increaseFavorite()
+    {
+        int currFav = getFavorite();
+        currFav++;
+        setFavorite(currFav);
+    }
+    // decrease # of favorites
+    public void decreaseFavorite()
+    {
+        int currFav = getFavorite();
+        if (currFav > 0)
+            currFav--;
+        setFavorite(currFav);
+    }
+
+    @DynamoDBAttribute(attributeName = "latitude")
+    // get latitude
+    public double getLatitude() { return latitude; }
+    // set longitude
+    public void setLatitude(double latitude) { this.latitude = latitude; }
+
+    @DynamoDBAttribute(attributeName = "longitude")
+    // get longitude
+    public double getLongitude() { return longitude; }
+    // set longitude
+    public void setLongitude(double longitude) { this.longitude = longitude; }
+
+    @DynamoDBAttribute(attributeName = "ownerId")
+    public String getOwner() { return ownerId; }
+    public void setOwner(String ownerId) { this.ownerId = ownerId; }
+
+    @DynamoDBAttribute(attributeName = "replyIds")
+    public ArrayList<int> getReplyIds() { return replyIds; }
+    public void setReplyIds(ArrayList<int> replies) { this.replyIds = replies; }
+    public int addReplyId(int id)
+    {
+        ArrayList<int> currIds = getReplyIds();
+        currIds.add(id);
+        setReplyIds(currIds);
+    }
+
+    // get replies
+    public ArrayList<Reply> getReplies() { return replies; }
+    // add replies
+    public void addToReplies(Reply reply)
+    {
+        replies.add(reply);
+        addReplyId(reply.getId());
+    }
+
+    @DynamoDBAttribute(attributeName = "subject")
+    // get subject
+    public String getSubject() { return subject; }
+    // set subject
+    public void setSubject(String subject) { this.subject = subject; }
+
+    @DynamoDBAttribute(attributeName = "timeout")
+    // get timeout
+    public int getTimeout() { return timeout; }
+    // set timeout
+    public void setTimeout(int timeout) { this.timeout = timeout; }
+
+    // increase timeout
+    public void increaseTimeout()
+    {
+        int currTimeout = getTimeout();
+        currTimeout++;
+        setTimeout(currTimeout);
+    }
+
+    // decrease timeout
+    public void decreaseTimeout()
+    {
+        int currTimeout = getTimeout();
+        if (currTimeout > 0)
+            currTimeout--;
+        setTimeout(currTimeout);
+    }
 }

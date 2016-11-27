4abb6d9c84469bfc7fb5463627bda20a2c791ec5
lucyzhang17 <lucyz17@ucla.edu>
2016-11-27 15:56:21 -0800

    Update Reply.java
    Incorporate Database Hooks

--- app/src/main/java/com/example/ningwang/momento/Reply.java
+++ app/src/main/java/com/example/ningwang/momento/Reply.java
@@ -2,50 +2,39 @@ package com.example.ningwang.momento;
 
 import java.sql.Timestamp;
 import java.util.Date;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+
 
 /**
  * Created by Songyan Xie on 2016/11/13.
  */
 // Reply Class
+
+@DynamoDBTable(tableName = "Replies")
 public class Reply{
-    public Reply(String ownerId, String content)
-    {
-        this.ownerId = ownerId;
-        this.content = content;
-        Date date = new Date();
-        timestamp = new Timestamp(date.getTime());
-        id = -1;
-    }
-
-    public Reply()
-    {
-        this.ownerId = "";
-        this.content = null;
-        Date date = new Date();
-        timestamp = new Timestamp(date.getTime());
-        id = -1;
-    }
-
-    public void setTimestamp()
-    {
-        Date date = new Date();
-        timestamp = new Timestamp(date.getTime());
-    }
-
-    public void setOwner(String ownerId) { this.ownerId = ownerId; }
-
-    public String getOwner() { return ownerId; }
-
-    public String getContent() { return content; }
-
-    public Timestamp getTimestamp() { return timestamp; }
-
-    public void setId(int id) { this.id = id; }
-
-    public int getId() { return id; }
-
     private String ownerId;
     private String content;
     private Timestamp timestamp;
     private int id;
+
+    @DynamoDBHashKey(attributeName = "replyId")
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    @DynamoDBAttribute(attributeName = "content")
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+
+    @DynamoDBAttribute(attributeName = "ownerId")
+    public String getOwner() { return ownerId; }
+    public void setOwner(String ownerId) { this.ownerId = ownerId; }
+
+    @DynamoDBAttribute(attributeName = "Timestamp")
+    public Timestamp getTimestamp() { return timestamp; }
+    public void setTimestamp()
+    {
+        Date date = new Date();
+        tp = new Timestamp(date.getTime());
+        this.timestamp = tp;
+    }
 }
